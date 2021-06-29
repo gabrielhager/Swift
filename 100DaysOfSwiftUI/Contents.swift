c07596b319617671862d6a35fc8d7edbc15377ea
gabrielhager <64944958+gabrielhager@users.noreply.github.com>
2021-06-29 14:14:25 -0700

    Day2
    Complex Data Types

--- 100DaysOfSwiftUI/Contents.swift
+++ 100DaysOfSwiftUI/Contents.swift
@@ -1,28 +1,60 @@
 import UIKit
 
-var greeting = "Hello, playground"
-var hello = "this is a string"
-var number = 1
-var million = 1_000_000
-var multiLine = """
-    this is on
-    multiple
-    lines
-    """
-var multiLine2 = """
-    this is on \
-    multiple \
-    lines
-    """
+// Day 2 Complex Data Types
 
-var doubleVSInt = 3.141
+//Array
+let dog = "Arwen"
+let cat = "Toad"
+let man = "Gabriel"
+let woman = "Bethany"
+let baby = "Rose"
 
-var str = "Pi is kind of equal to \(doubleVSInt)"
+let family = [dog, cat, man, woman, baby]
+family[0]
 
-var str2 = "Look at me \(str)"
-let myGender = "male"
-//myGender = "female" not allowed!
+//Sets
+let idNumbers = Set([1, 2, 3, 3, 4, 5, 6, 6, 7])
 
-var percent : Double = 99
-var apple : String
-apple = "Mac"
+//Tuples
+let info = (name: "Gabe" , age: 29, male: true)
+info.name
+info.age
+info.male
+
+//Dictionary
+var animalFacts = [
+    "dog" : "a four legged mammal",
+    "cat" : "like a dog, only sharper",
+    "snake" : "nope rope"
+]
+animalFacts["snake"]
+
+//Dictionary Default Value
+animalFacts["gopher", default: "not in dictionary" ]
+
+// Empty Sets
+var emptyArray = [String]()
+var emptyDict = [String: Int]()
+var emptySet = Set<String>()
+
+emptyDict["First"] = 1
+
+//Enums
+enum Result {
+    case success(praise : Double)
+    case failure(scorn : String)
+}
+let result1 = Result.success(praise: 100.0)
+let result2 = Result.failure(scorn: "you suck")
+
+enum valueMan : Int{
+    case zero
+    case one
+    case two
+}
+let valueZero = valueMan(rawValue: 0)
+
+
+
+
+
