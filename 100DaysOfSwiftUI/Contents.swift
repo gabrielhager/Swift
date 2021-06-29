c6f920c45de0c42cca631c15d48b04753a25d7ba
gabrielhager <64944958+gabrielhager@users.noreply.github.com>
2021-06-29 14:13:07 -0700

    Day1

---
+++ 100DaysOfSwiftUI/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var hello = "this is a string"
+var number = 1
+var million = 1_000_000
+var multiLine = """
+    this is on
+    multiple
+    lines
+    """
+var multiLine2 = """
+    this is on \
+    multiple \
+    lines
+    """
+
+var doubleVSInt = 3.141
+
+var str = "Pi is kind of equal to \(doubleVSInt)"
+
+var str2 = "Look at me \(str)"
+let myGender = "male"
+//myGender = "female" not allowed!
+
+var percent : Double = 99
+var apple : String
+apple = "Mac"
